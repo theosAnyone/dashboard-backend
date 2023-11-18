314eff407e4332c43a034e0b1b8d2efbecd17cd0
theosAnyone <theo.fernandez.perso@gmail.com>
2023-11-18 21:00:49 +0100

    add review teacher

--- controllers/authController.js
+++ controllers/authController.js
@@ -9,7 +9,7 @@ const asyncHandler = require('express-async-handler')
 // @acces Public
 const login = asyncHandler(async (req, res) => {
     const {email, password, } = req.body;
-
+    console.log("body:",req.body);
     if (!email || !password ) {
         return res.status(400).json({message: 'All fields are required'})
     }
